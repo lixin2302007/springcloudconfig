97f3843c0319024ca30f407c78e3c67d0fda93cf
lixin2302007 <31731732+lixin2302007@users.noreply.github.com>
2018-12-19 18:51:20 +0800

    Add files via upload

---
+++ spring-cloud-config-demo/src/main/java/com/cloud/config/demo/SpringCloudConfigDemoApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.config.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class SpringCloudConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigDemoApplication.class, args);
+	}
+
+}
+
