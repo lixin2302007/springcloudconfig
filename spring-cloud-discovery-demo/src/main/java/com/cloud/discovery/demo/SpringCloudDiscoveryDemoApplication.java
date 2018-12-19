5d463b24e8a1d3cebdd4bea7180711bfee6f916f
lixin2302007 <31731732+lixin2302007@users.noreply.github.com>
2018-12-19 18:50:26 +0800

    Add files via upload

---
+++ spring-cloud-discovery-demo/src/main/java/com/cloud/discovery/demo/SpringCloudDiscoveryDemoApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.discovery.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudDiscoveryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDiscoveryDemoApplication.class, args);
+	}
+
+}
+
---
+++ spring-cloud-discovery-demo/src/test/java/com/cloud/discovery/demo/SpringCloudDiscoveryDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cloud.discovery.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudDiscoveryDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
